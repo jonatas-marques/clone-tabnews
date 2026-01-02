e3c161a8cc8257327d9e5c91d25b56a255bb0906
jonatas-marques <marjonatas@gmail.com>
2026-01-02 19:29:14 -0300

    fix: linting with prettier

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,16 +2,16 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
-  // Obtém a versão do banco de dados
+
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
-  // Obtém o número máximo de conexões do banco de dados
+
   const databaseMaxConnectionsResult = await database.query(
     "SHOW max_connections;"
   );
   const databaseMaxConnectionsValue =
     databaseMaxConnectionsResult.rows[0].max_connections;
-  // Obtém o número de conexões abertas no banco de dados
+
   const databaseName = process.env.POSTGRES_DB;
   const databaseOpenConnectionsResult = await database.query({
     text: "SELECT COUNT(*) FROM pg_stat_activity WHERE datname = $1;",
