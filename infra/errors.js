f73cae05fb532d28130ad296912f20180e8d372a
Jonatas Marques <33204474+jonatas-marques@users.noreply.github.com>
2026-01-08 22:31:50 +0000

    feat: create and use `InternalServerError` in `api/v1/status`

---
+++ infra/errors.js
@@ -0,0 +1,19 @@
+import { act } from "react";
+
+export class InternalServerError extends Error {
+  constructor({ cause }) {
+    super("Um erro interno não esperado aconteceu.", {
+      cause,
+    });
+    this.name = "InternalServerError";
+    this.action = "Entre em contato com o suporte.";
+  }
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.status_code,
+    };
+  }
+}
