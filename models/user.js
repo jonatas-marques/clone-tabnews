bda57adfa6880c8e2ce6b93a02c4351fafff938f
Jonatas Marques <33204474+jonatas-marques@users.noreply.github.com>
2026-01-13 02:27:26 +0000

    feat: create `user.findOneByUsername` method and `/api/v1/users/[username]` endpoint

--- models/user.js
+++ models/user.js
@@ -1,5 +1,36 @@
 import database from "infra/database.js";
-import { ValidationError } from "infra/errors.js";
+import { ValidationError, NotFoundError } from "infra/errors.js";
+
+async function findOneByUsername(username) {
+  const userFound = await runSelectQuery(username);
+  return userFound;
+
+  async function runSelectQuery(username) {
+    const results = await database.query({
+      text: `
+      SELECT 
+        * 
+      FROM 
+        users
+      WHERE
+        LOWER(username) = LOWER($1)
+      LIMIT
+        1
+      ;`,
+      values: [username],
+    });
+
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message: "Usuário não encontrado.",
+        action: "Verifique o nome de usuário informado.",
+        status_code: 404,
+      });
+    }
+
+    return results.rows[0];
+  }
+}
 
 async function create(userImputValues) {
   await validateUniqueEmail(userImputValues.email);
@@ -68,5 +99,6 @@ async function create(userImputValues) {
 }
 const user = {
   create,
+  findOneByUsername,
 };
 export default user;
