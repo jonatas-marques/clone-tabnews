7cb7da0502f53eed38173ddfeee3842963ce9f37
Jonatas Marques <33204474+jonatas-marques@users.noreply.github.com>
2026-01-25 02:33:29 +0000

    feat: implement session deletion

--- models/session.js
+++ models/session.js
@@ -85,10 +85,34 @@ async function renew(sessionId) {
   }
 }
 
+async function expireByID(sessionId) {
+  const expiredSessionObject = runUpdateQuery(sessionId);
+  return expiredSessionObject;
+
+  async function runUpdateQuery(sessionId) {
+    const results = await database.query({
+      text: `
+       UPDATE
+          sessions
+        SET
+          expires_at = expires_at - interval '1 year',
+          updated_at = NOW()
+        WHERE
+          id = $1
+        RETURNING
+          *
+        ;`,
+      values: [sessionId],
+    });
+    return results.rows[0];
+  }
+}
+
 const session = {
   create,
   findOneValidByToken,
   renew,
+  expireByID,
   EXPIRATION_IN_MILLISECONDS,
 };
 
