fe179ebc9c342e22a34bac51fe8709fe69c931a0
Jonatas Marques <33204474+jonatas-marques@users.noreply.github.com>
2026-03-26 16:34:09 +0000

    test: fix ISO date format in `authorization` unit tests

--- tests/unit/models/authorization.test.js
+++ tests/unit/models/authorization.test.js
@@ -70,8 +70,8 @@ describe("models/authorization.js", () => {
         id: 1,
         username: "ResourceUser",
         features: ["read:user"],
-        created_at: "2026-0101T00:00:00.000Z",
-        updated_at: "2026-0101T00:00:00.000Z",
+        created_at: "2026-01-01T00:00:00.000Z",
+        updated_at: "2026-01-01T00:00:00.000Z",
         email: "resource@test.com",
         password: "resource",
       };
@@ -85,8 +85,8 @@ describe("models/authorization.js", () => {
         id: 1,
         username: "ResourceUser",
         features: ["read:user"],
-        created_at: "2026-0101T00:00:00.000Z",
-        updated_at: "2026-0101T00:00:00.000Z",
+        created_at: "2026-01-01T00:00:00.000Z",
+        updated_at: "2026-01-01T00:00:00.000Z",
       });
     });
   });
